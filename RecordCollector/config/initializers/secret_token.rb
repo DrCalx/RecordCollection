09efd8d923b6f401864fe763beb88f85c0de3b6d
Matthew Duguay <matthew.duguay@gmail.com>
2014-05-23 21:38:21 -0400

    Moving this thing to Rails for practice

---
+++ RecordCollector/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecordCollector::Application.config.secret_key_base = '6637486a794e92013c925d216fa60d7d1768e027f4bf6f1f3b10d17cedbd0c652176c164e001be80bfb6b3ffde80a9758380b6e18c5cfc6feb98a9ca51e9620f'
